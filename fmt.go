af4280b5a741a50cdba54796ce5b6c092071414b
xxarupakaxx <arupaka3tdigks@gmail.com>
2020-09-01 23:50:48 +0900

    var 変数

--- fmt.go
+++ fmt.go
@@ -2,6 +2,22 @@ package main
 
 import "fmt"
 
+var n = 100
+
+func main() {
+	n = n + 1
+	fmt.Printf("n=%d\n", n)
+}
+
+/*
 func main() {
 	fmt.Printf("10進数=%d, %b, %o,%x\n", 17, 17, 17, 17)
 }
+
+var n int
+var x,y,z int
+
+var {
+	x, y int
+	name string
+}*/

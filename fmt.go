c28bd87d1c59b48b20ff5e90e221f4cb2dcd7491
xxarupakaxx <arupaka3tdigks@gmail.com>
2020-09-02 11:33:18 +0900

    add

--- fmt.go
+++ fmt.go
@@ -2,14 +2,22 @@ package main
 
 import "fmt"
 
+/*
 var n = 100
+var c = complex(1.0, 3)
+var a =[5]int{1,2,3,4,5}
+
+func plus(x,y int) int {
+	return x + y
+}
 
 func main() {
 	n = n + 1
 	fmt.Printf("n=%d\n", n)
+	fmt.Printf("c=%v\n", c)
 }
 
-/*
+
 func main() {
 	fmt.Printf("10進数=%d, %b, %o,%x\n", 17, 17, 17, 17)
 }
@@ -21,3 +29,13 @@ var {
 	x, y int
 	name string
 }*/
+
+func div(x, y int) (int, int) {
+	q := x / y
+	r := x % y
+	return q, r
+}
+func main() {
+	q, r := div(19, 7)
+	fmt.Printf("%d,%d\n", q, r)
+}
--- zoo/app_test.go
+++ zoo/app_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func testAppName(t *testing.T) {
 	expect := "Zoo Application"
-	actual := AppName()
+	actual := appName()
 
 	if expect != actual {
 		t.Errorf("%s != %s", expect, actual)

fbacb388ffedd4dfd9d17a646f7d44a09db68a32
xxarupakaxx <arupaka3tdigks@gmail.com>
2020-09-03 00:33:16 +0900

    interface{}

--- app.go
+++ app.go
@@ -2,10 +2,20 @@ package main
 
 import "fmt"
 
+func anything(a interface{}) {
+	fmt.Println(a)
+}
+
 func main() {
 	fruits := [3]string{"apple", "baana", "cherry"}
 
 	for i, s := range fruits {
 		fmt.Printf("fruits[%d]=%s\n", i, s)
 	}
+
+	anything(1)
+	anything(3.14)
+	anything(1 + 3.0i)
+	anything("海")
+
 }

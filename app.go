3272347c9f91f4fda6ee5b96eb0f2589de5ec810
xxarupakaxx <arupaka3tdigks@gmail.com>
2020-09-03 00:36:57 +0900

    defer

--- app.go
+++ app.go
@@ -2,20 +2,12 @@ package main
 
 import "fmt"
 
-func anything(a interface{}) {
-	fmt.Println(a)
+func runDefer() {
+	defer fmt.Println("defer")
+	fmt.Println("de")
 }
 
 func main() {
-	fruits := [3]string{"apple", "baana", "cherry"}
-
-	for i, s := range fruits {
-		fmt.Printf("fruits[%d]=%s\n", i, s)
-	}
-
-	anything(1)
-	anything(3.14)
-	anything(1 + 3.0i)
-	anything("海")
+	runDefer()
 
 }

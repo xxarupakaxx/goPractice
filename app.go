6f787a3e1b02f2d90924cd1dd8b7364785f56058
xxarupakaxx <arupaka3tdigks@gmail.com>
2020-09-03 16:10:12 +0900

    mapmake

--- app.go
+++ app.go
@@ -3,6 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	m := map[int]string{1: "Taro", 2: "Hanako", 3: "jiro"}
-	fmt.Println(m)
+	m := map[string]int{
+		"Apple":  88,
+		"Banana": 107,
+		"vherry": 46,
+	}
+	m["Grape"] = 66
+	m["Lemon"] = 16
+	m["Orange"] = 44
+	m["Oineapple"] = 73
+	for k, v := range m {
+		fmt.Printf("%v => %v\n", k, v)
+	}
 }

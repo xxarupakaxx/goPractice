7830f6451f01ef87c12baeac7f64dbe21ec9c2d1
xxarupakaxx <arupaka3tdigks@gmail.com>
2020-09-03 12:51:52 +0900

    map

--- app.go
+++ app.go
@@ -1,14 +1,14 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	s := make([]int, 0, 0)
-	fmt.Printf("len = %d , cap = %d\n", len(s), cap(s))
-	s = append(s, 1)
-	fmt.Printf("len = %d , cap = %d\n", len(s), cap(s))
-	s = append(s, []int{2, 3, 4}...)
-	fmt.Printf("len = %d , cap = %d\n", len(s), cap(s))
+	m := make(map[int]string)
+
+	m[1] = "US"
+	m[81] = "Japan"
+	m[86] = "China"
+
+	fmt.Println(m)
+
 }

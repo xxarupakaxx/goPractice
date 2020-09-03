76ef2aa152f9d51e05d4a7f000777d7da76ef7c6
xxarupakaxx <arupaka3tdigks@gmail.com>
2020-09-03 10:10:48 +0900

    defer recover

--- app.go
+++ app.go
@@ -2,13 +2,27 @@ package main
 
 import "fmt"
 
-func main() {
+func testRecover(src interface{}) {
 	defer func() {
 		if x := recover(); x != nil {
-			fmt.Println(x)
+			switch v := x.(type) {
+			case int:
+				fmt.Printf("panic: int= %v\n", v)
+			case string:
+				fmt.Printf("panic: string= %v\n", v)
+			default:
+				fmt.Printf("panic unknown")
+			}
 		}
-	}()
-	panic("panic")
 
-	fmt.Println("jhe")
+	}()
+	panic(src)
+	return
+}
+
+func main() {
+	testRecover(128)
+	testRecover("hogehoge")
+	testRecover([...]int{1, 2, 3})
+
 }

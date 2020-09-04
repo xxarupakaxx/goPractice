f1eed91f171e7b36cff5f8a362e35afde6e3acfb
xxarupakaxx <arupaka3tdigks@gmail.com>
2020-09-04 23:04:54 +0900

    select

--- app.go
+++ app.go
@@ -3,10 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	ch := make(chan int, 3)
-	ch <- 1
-	ch <- 2
-	for i := range ch {
-		fmt.Println(i)
+	ch1 := make(chan int, 1)
+	ch2 := make(chan int, 1)
+	ch3 := make(chan int, 1)
+	ch1 <- 1
+	ch2 <- 2
+
+	select {
+	case <-ch1:
+		fmt.Println("ch1")
+	case <-ch2:
+		fmt.Println("ch2")
+	case <-ch3:
+		fmt.Println("ch3")
+	default:
+		fmt.Println("NO")
 	}
 }

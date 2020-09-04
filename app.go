93062de047b4044cf27429b83f87bd55f30245fd
xxarupakaxx <arupaka3tdigks@gmail.com>
2020-09-04 23:02:01 +0900

    制御文とチャネル

--- app.go
+++ app.go
@@ -1,36 +1,12 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
-
-func receive(name string, ch <-chan int) {
-	for {
-		i, ok := <-ch
-		if ok == false {
-			break
-		}
-		fmt.Println(name, i)
-
-	}
-	fmt.Println(name + "is done")
-}
+import "fmt"
 
 func main() {
-	ch := make(chan int, 20)
-
-	go receive("1st goroutine ", ch)
-	go receive("2nd goroutine ", ch)
-	go receive("3rd goroutine ", ch)
-
-	i := 0
-	for i < 100 {
-		ch <- i
-		i++
+	ch := make(chan int, 3)
+	ch <- 1
+	ch <- 2
+	for i := range ch {
+		fmt.Println(i)
 	}
-	close(ch)
-
-	time.Sleep(3 * time.Second)
-
 }

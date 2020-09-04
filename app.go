c7f81c4708e00bb9236a4d98cf2623535b04e3f7
xxarupakaxx <arupaka3tdigks@gmail.com>
2020-09-04 20:41:13 +0900

    update goの動作確認

--- app.go
+++ app.go
@@ -1,24 +1,7 @@
 package main
 
-import "fmt"
-
-func receiver(ch <-chan int) {
-	for {
-		i := <-ch
-		fmt.Println(i)
-	}
-}
-
 func main() {
 
-	ch := make(chan int)
-
-	go receiver(ch)
-
-	i := 0
-	for i < 10 {
-		ch <- i
-		i++
-	}
+	println("hello,world")
 
 }

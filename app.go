96bd310d2de1d3f624afd7a3780041b368a31fb2
xxarupakaxx <arupaka3tdigks@gmail.com>
2020-09-04 22:43:34 +0900

    goroutineとチャネル

--- app.go
+++ app.go
@@ -1,7 +1,36 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"time"
+)
 
-	println("hello,world")
+func receive(name string, ch <-chan int) {
+	for {
+		i, ok := <-ch
+		if ok == false {
+			break
+		}
+		fmt.Println(name, i)
+
+	}
+	fmt.Println(name + "is done")
+}
+
+func main() {
+	ch := make(chan int, 20)
+
+	go receive("1st goroutine ", ch)
+	go receive("2nd goroutine ", ch)
+	go receive("3rd goroutine ", ch)
+
+	i := 0
+	for i < 100 {
+		ch <- i
+		i++
+	}
+	close(ch)
+
+	time.Sleep(3 * time.Second)
 
 }

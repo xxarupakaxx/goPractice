7cc349a969dbabd49f240aa0e8fe857802bf0387
xxarupakaxx <arupaka3tdigks@gmail.com>
2020-09-04 23:31:32 +0900

    chとselect

--- app.go
+++ app.go
@@ -3,20 +3,37 @@ package main
 import "fmt"
 
 func main() {
-	ch1 := make(chan int, 1)
-	ch2 := make(chan int, 1)
-	ch3 := make(chan int, 1)
-	ch1 <- 1
-	ch2 <- 2
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+	ch3 := make(chan int)
 
-	select {
-	case <-ch1:
-		fmt.Println("ch1")
-	case <-ch2:
-		fmt.Println("ch2")
-	case <-ch3:
-		fmt.Println("ch3")
-	default:
-		fmt.Println("NO")
+	go func() {
+		for {
+			i := <-ch1
+			ch2 <- (i * 2)
+		}
+	}()
+
+	go func() {
+		for {
+			i := <-ch2
+			ch3 <- (i - 1)
+		}
+	}()
+
+	n := 1
+LOOP:
+	for {
+		select {
+		case ch1 <- n:
+			n++
+		case i := <-ch3:
+			fmt.Println("received", i)
+		default:
+			if n > 100 {
+				break LOOP
+			}
+		}
 	}
+
 }

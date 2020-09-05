4c712594a47eb550c9a52a153c317694f412cae7
xxarupakaxx <arupaka3tdigks@gmail.com>
2020-09-05 12:10:33 +0900

    callbuck type

--- app.go
+++ app.go
@@ -2,12 +2,28 @@ package main
 
 import "fmt"
 
+type (
+	IntPair     [2]int
+	Strings     []string
+	AreaMap     map[string][2]float64
+	IntsChannel chan []int
+	CallBack    func(i int) int
+)
+
+func Sum(ints []int, CallBack CallBack) int {
+	var sum int
+	for _, i := range ints {
+		sum += i
+
+	}
+	return CallBack(sum)
+}
 func main() {
-	type MyInt int
-
-	var n1 MyInt = 5
-	n2 := MyInt(7)
-	fmt.Println(n1)
-	fmt.Println(n2)
-
+	n := Sum(
+		[]int{1, 2, 3, 4, 5},
+		func(i int) int {
+			return i * 2
+		},
+	)
+	fmt.Println(n)
 }

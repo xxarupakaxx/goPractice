744ee8cc402ec9479053bb041a15ad7eb93b4347
xxarupakaxx <arupaka3tdigks@gmail.com>
2020-09-05 15:16:17 +0900

    new

--- app.go
+++ app.go
@@ -2,28 +2,14 @@ package main
 
 import "fmt"
 
-type (
-	IntPair     [2]int
-	Strings     []string
-	AreaMap     map[string][2]float64
-	IntsChannel chan []int
-	CallBack    func(i int) int
-)
-
-func Sum(ints []int, CallBack CallBack) int {
-	var sum int
-	for _, i := range ints {
-		sum += i
-
-	}
-	return CallBack(sum)
+type Person struct {
+	Id int
+	Name string
+	Area string
 }
-func main() {
-	n := Sum(
-		[]int{1, 2, 3, 4, 5},
-		func(i int) int {
-			return i * 2
-		},
-	)
-	fmt.Println(n)
-}
+
+p := new(Person)
+
+p.Id
+p.Name
+p.Area

003e3c10df0c96543b5d884a903383dbe8c6ed6e
xxarupakaxx <arupaka3tdigks@gmail.com>
2020-09-13 20:56:28 +0900

    Mapは避ける

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
+
+//KeyValueのための型内部にMaｐを所持
+type KeyValue struct {
+	store map[string]string //keyValue を格納
+	mu    sync.RWMutex      //排他制御のためのmutex
+}
+
+func NewKeyValue() *KeyValue {
+	return &KeyValue{store: make(map[string]string)}
+}
+func (kv *KeyValue) Set(key, val string) {
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+	kv.store[key] = val
+}
+func (kv *KeyValue) Get(key string) (string, bool) {
+	kv.mu.RLock()
+	defer kv.mu.RUnlock()
+	val, ok := kv.store[key]
+	return val, ok
+}
 
 func main() {
-	fmt.Println("hello")
+	kv := NewKeyValue()
+	kv.Set("key", "value")
+	value, ok := kv.Get("key")
+	if ok {
+		fmt.Println(value)
+	}
 }

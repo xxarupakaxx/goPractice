df003308db6ff71578c934e7da4235d6ae59330f
xxarupakaxx <arupaka3tdigks@gmail.com>
2020-09-13 14:12:49 +0900

    math

--- main.go
+++ main.go
@@ -1,32 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
-type Base struct {
-	Id int 
-	Owner string
+func showStruct(s struct{ X, Y int }) {
+	fmt.Println(s)
 }
-type A struct {
-	Base 
-	Name string
-	Area string
-}
-type B struct {
-	Base
-	Title string
-	Bodies []string
-}
-a := A{
-	Base : Base{17,"Taro"},
-	Name :"Taro",
-	Area : "Tokyo",
+
+type Point struct{ X, Y int }
+
+func (p *Point) Distance(dp *Point) float64 {
+	x, y := p.X-dp.X, p.Y-dp.Y
+	return math.Sqrt(float64(x*x + y*y))
 }
 func main() {
-	var p *int
-	fmt.Println(p == nil)
-	var i int
-	a := &i
-	fmt.Printf("%T\n", a)
-	pp := &a
-	fmt.Printf("%T\n", pp)
+	p := Point{1, 2}
+	fmt.Println(p.Distance(&Point{0, 0}))
+
 }

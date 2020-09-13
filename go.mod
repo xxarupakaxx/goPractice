4bc351b2ad9a875f766147a5947fa8ac0baf6e31
xxarupakaxx <arupaka3tdigks@gmail.com>
2020-09-13 19:59:15 +0900

    module

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/xxarupakaxx/goPractice
+
+go 1.15
--- main.go
+++ main.go
@@ -1,51 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"math"
-)
-type MyError struct{
-	Message string
-	ErrCode int
-}
-func (e *MyError) Error() string {
-	return e.Message
-}
-func RaiseError()error{
-	return MyError{Message:"error",ErrCode:1234}
-}
-err
-type User struct {
-	Id   int    "ユーザー"
-	Name string "名前"
-	Age  uint   "年齢"
-}
+import "fmt"
 
-func showStruct(s struct{ X, Y int }) {
-	fmt.Println(s)
-}
-
-type Point struct{ X, Y int }
-
-func (p *Point) Distance(dp *Point) float64 {
-	x, y := p.X-dp.X, p.Y-dp.Y
-	return math.Sqrt(float64(x*x + y*y))
-}
-func (p *Point) ToString() string {
-	return fmt.Sprintf("%d,%d", p.X, p.Y)
-}
-
-type MyInt int
-
-func (p Point) Set(x, y int) {
-	p.X = x
-	p.Y = y
-
-}
-
-func (m MyInt) Pluse(i int) int {
-	return int(m) + 1
-}
 func main() {
-
+	fmt.Println("hello")
 }
